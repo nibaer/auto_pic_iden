7d068b67255c6ac238c249f7be9b1aa28d52610a
nibaer <luckylz2009@gmail.com>
2020-03-16 11:22:17 +0100

    back

--- manage.py
+++ manage.py
@@ -16,12 +16,6 @@ def main():
         ) from exc
     execute_from_command_line(sys.argv)
 
-    hallo from branch
 
 if __name__ == '__main__':
     main()
-    
-    branch finished!
-
-    master end!
-
